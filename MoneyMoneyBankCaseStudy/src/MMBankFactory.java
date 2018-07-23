aadecdf9b638309529ec98d7c74720fd4164479d
Subhamoy Sarkar <30719940+samwisemj@users.noreply.github.com>
2018-07-23 17:22:10 +0000

    Update MMBankFactory.java

--- MoneyMoneyBankCaseStudy/src/MMBankFactory.java
+++ MoneyMoneyBankCaseStudy/src/MMBankFactory.java
@@ -4,8 +4,9 @@ public class MMBankFactory extends BankFactory{
 	private static int uniqueAcNo=10000;
 	@Override
 	
-	protected CurrentAccount getNewCurrentAccount() {
-		System.out.println("Open a new Current account in MoneyMoney Bank!!! Please furnish name, initial deposit, creditLimt");
+	protected CurrentAccount getNewCurrentAccount(int accName,float accBal, float creditLimit ) {
+		
+		
 		return null;
 	}
 }
