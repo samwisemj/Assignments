61ac454352d1d8140fd5ff4eff1448b10fa8d3e0
Subhamoy Sarkar <30719940+samwisemj@users.noreply.github.com>
2018-07-23 17:36:25 +0000

    Update MMBankFactory.java

--- MoneyMoneyBankCaseStudy/src/MMBankFactory.java
+++ MoneyMoneyBankCaseStudy/src/MMBankFactory.java
@@ -1,14 +1,53 @@
 
 public class MMBankFactory extends BankFactory{
 
-	private static int uniqueAcNo=10000;
+	private static int uniqueCAcNo=10000;
+	  private static int uniqueSAcNo=50000;
 	@Override
 	
 	protected CurrentAccount getNewCurrentAccount(int accName,float accBal, float creditLimit ) { 
-		CurrentAccount currentAccount = new CurrentAccount(uniqueAcNo,accName,accBal,creditLimit);
-		uniqueAcNo++;
+		CurrentAccount currentAccount = new CurrentAccount(uniqueCAcNo,accName,accBal,creditLimit);
+		uniqueCAcNo++;
 		
 		
 		return currentAccount;
 	}
+	
+	
+   @Override
+
+
+
+        
+
+
+
+        protected SavingsAccount getNewSavingsAccount(int accName,float accBal, boolean isSalaried ) { 
+
+
+
+                SavingsAccount SavingsAccount = new SavingsAccount(uniqueSAcNo,accName,accBal,isSalaried);
+			
+
+
+
+uniqueSAcNo++;
+
+
+
+                
+
+
+
+                
+
+
+
+                return savingsAcount;
+
+
+
+        }
+	
+	
 }
