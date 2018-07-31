8997905daeee3507907c519fa6ed8f52f2db7239
Sarkar <subhamoy.sarkar@capgemini.com>
2018-07-31 17:32:09 +0530

    Assignments as of 31/07/2018

--- FileIO Assignments/src/com/capgemini/javagrep/JavaGrepApplication.java
+++ FileIO Assignments/src/com/capgemini/javagrep/JavaGrepApplication.java
@@ -4,20 +4,21 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class JavaGrepApplication {
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println(
-				"Enter the name of the file (Hint: Enter TestFile.txt to test the text file present in the project)");
+				"Enter the name of the file (Hint: Enter TestFile.txt to test the text file present in the project)\n also enter the search element");
 
-		String path = br.readLine();
+		String path = args[0];
 //		String path = "TestFile.txt";
+		
 		int occurenceCount = 0;
 		int lineCount = 0;
+		
 		String line = null;
+		
 		// creating a file object with the file path specified
 		File file = new File(path);
 
@@ -27,8 +28,8 @@ public class JavaGrepApplication {
 			System.exit(1);
 		}
 
-		System.out.println("Enter the word to be searched");
-		String element = br.readLine();
+		//System.out.println("Enter the word to be searched");
+		String element = args[1];
 
 		// file reader object with the file object
 		FileReader fr = new FileReader(file);
@@ -51,7 +52,6 @@ public class JavaGrepApplication {
 		if (occurenceCount == 0)
 				System.out.println("The word was not found in the document");
 			reader.close();
-
 	}
 
 }
